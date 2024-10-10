9bb9d4e5c661e379bcd3528c2eb61daad2dd8636
andzh1 <andrey.zhizhelev@yandex.ru>
2024-10-10 22:21:45 +0300

    upd debug detection

--- constants.py
+++ constants.py
@@ -1,11 +1,8 @@
 import os
 
-import subprocess
-
-current_user = subprocess.check_output(['whoami']).decode().strip()
-DEBUG = False if current_user == 'phys-bot' else True
 workdir=os.path.dirname(os.path.abspath(__file__))
-print(DEBUG)
+DEBUG = 'andrew' in workdir
+
 responses_db_name = f'{workdir}/user_responses{"_test" if DEBUG else ""}.db'
 users_db_name = f'{workdir}/users{"_test" if DEBUG else ""}.db'
 token_key = f"physbot-{"test" if DEBUG else "main"}-token"
