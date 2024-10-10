ac1074e69690e76288be05ddbd243c215dc87b9f
andzh1 <andrey.zhizhelev@yandex.ru>
2024-10-10 22:14:21 +0300

    Add debug detection

--- constants.py
+++ constants.py
@@ -1,8 +1,11 @@
 import os
 
-DEBUG = True
-workdir=os.path.dirname(os.path.abspath(__file__))
+import subprocess
 
+current_user = subprocess.check_output(['whoami']).decode().strip()
+DEBUG = False if current_user == 'phys-bot' else True
+workdir=os.path.dirname(os.path.abspath(__file__))
+print(DEBUG)
 responses_db_name = f'{workdir}/user_responses{"_test" if DEBUG else ""}.db'
 users_db_name = f'{workdir}/users{"_test" if DEBUG else ""}.db'
 token_key = f"physbot-{"test" if DEBUG else "main"}-token"
