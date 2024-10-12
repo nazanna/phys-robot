11658f6492e3622c52a52468506264509f448478
andzh1 <andrey.zhizhelev@yandex.ru>
2024-10-12 12:02:38 +0300

    ficha

--- questions.py
+++ questions.py
@@ -1,9 +1,12 @@
 import pandas as pd
 from constants import workdir
 
-df = pd.read_csv(f'{workdir}/questions_7_8_grade.csv')
-QUESTIONS = df['тема'].tolist()
+df = pd.read_csv(f"{workdir}/questions_7_8_grade.csv")
+QUESTIONS = df["тема"].tolist()
 
-IMAGES = [f'{workdir}/Problems/'+'00'+str(num)+'.png' for num in range(1, 10)]
+IMAGES = [f"{workdir}/Problems/" + "00" + str(num) + ".png" for num in range(1, 10)]
 for num in range(10, len(QUESTIONS) + 1):
-    IMAGES.append(f'{workdir}/Problems/'+'0'+str(num)+'.png')
+    if num < 100:
+        IMAGES.append(f"{workdir}/Problems/" + "0" + str(num) + ".png")
+    else:
+        IMAGES.append(f"{workdir}/Problems/" + str(num) + ".png")
